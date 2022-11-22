5ddde9e6f655aca780bf21fc4bfb1b8524bec9c4
IngarKo <116308643+IngarKo@users.noreply.github.com>
2022-11-22 22:32:18 +0200

    navbar

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -1,5 +1,8 @@
 import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Avaleht from './pages/Avaleht';
 import LisaToode from './pages/LisaToode';
 import Ostukorv from './pages/Ostukorv';
@@ -13,10 +16,31 @@ import YksikToode from './pages/YksikToode';
 
 function App() {
   return (
-    <div className="App">
-      <Link to="/">
+<div className="App">
+    <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Veebileht</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/ostukorv">Ostukorv</Nav.Link>
+            <Nav.Link as={Link} to="/lisa-toode">Lisa toode</Nav.Link>
+            <Nav.Link as={Link} to="/seaded">Seaded</Nav.Link>
+            <Nav.Link as={Link} to="/meist">Meist</Nav.Link>
+            <Nav.Link as={Link} to="/poed">Poed</Nav.Link>
+            <Nav.Link as={Link} to="/tooted">Tooted</Nav.Link>
+            <Nav.Link as={Link} to="/halda">Halda tooteid</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+
+
+
+
+
+    
+      {/*<Link to="/">
       <img className="main-pilt"  src="https://apod.nasa.gov/apod/image/2202/AuroraPillars_Correia_4898.jpg" alt=""  />
-      </Link>
+  </Link>
       <Link to="/ostukorv">
         <button className="nupp">ostukorv</button>
       </Link>
@@ -37,8 +61,8 @@ function App() {
       </Link>
       <Link to="/halda">
         <button className="nupp">Halda tooteid</button>
-      </Link>
-
+      </Link>*/}
+      <img className="main-pilt"  src="https://apod.nasa.gov/apod/image/2202/AuroraPillars_Correia_4898.jpg" alt=""  />
       <Routes>
         <Route path="" element={ <Avaleht /> } />
         <Route path="ostukorv" element={ <Ostukorv /> } />
